7f549dc47e9004e3e6facc9a4fb3dcd9ed26fb00
Jaydon Lee <96518885+Hyukjoo-Lee@users.noreply.github.com>
2023-10-03 23:47:45 -0700

    Time: 0 ms (100%), Space: 39.5 MB (14.99%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,39 +1,39 @@
 class Solution {
-    /**
-        return how many distinct ways you can reach the top
-        able to climb one or two steps at once
-        dynamic programming algorithm
-        we already know 
-        dp[1] = 1, dp[2] = 2
-        therefore, dp[3] = dp[1] + dp[2] = 3
-        dp[4] = dp[2] + dp[3]
-        dp[n] = dp[n-2] + dp[n-1]
-     */
     public int climbStairs(int n) {
-        if (n == 1) return 1;
-        if (n == 2) return 2;
-        // if n = 3,
+        /**
+            n -> the number of stairs
+            top = n + 1
+            each times 1 or 2 steps
+            distinct ways? 
+            dynamic programming
+            we already know dp[1] = 1, dp[2] = 2
+            dp[n] = dp[n-2] + dp[n-1]
+            dp[3] = dp[1] + dp[2] = 3 ways
+            dp[4] = dp[2] + dp[3] = 5 ways
+         */
+        // 1 <= n <= 45
+        
+        if(n == 1) return 1;
+        if(n == 2) return 2;
+
         int[] dp = new int[n+1];
 
-        dp[0] = 0;
+        // base cases
         dp[1] = 1;
         dp[2] = 2;
-
+        // when i is 4, stop and return dp[n](the number of distinct ways)
         for(int i = 3; i <= n; i++) {
-            dp[i] = dp[i-2] + dp[i-1]; // dp[3] = dp[1] + dp[2]
+            dp[i] = dp[i-2] + dp[i-1];
         }
 
         return dp[n];
+
+
+
+
+
+
+
+
     }
-
-
-
-
-
-
-
-
-
-
-
 }
