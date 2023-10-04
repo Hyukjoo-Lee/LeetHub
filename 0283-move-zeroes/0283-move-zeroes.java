5c7524a50ace82bb4b5c2e27ff537f31c432d92e
Jaydon Lee <96518885+Hyukjoo-Lee@users.noreply.github.com>
2023-10-04 03:02:03 -0700

    Time: 1 ms (100%), Space: 44.7 MB (88.68%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,31 +1,15 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-
-
-        
-        // two pointer
-        // first pointer will track non-zero values in the array
-        // second pointer will track where the non-zero value be placed
-
-        int pointer = 0;
-
-        // [0,1,0,3,12]
-        // i = 1 , pointer = 0
-        // [1,0,0,3,12]
-        // i = 2 , pointer = 1
-        // i = 3 , pointer = 1
-        // [1,3,0,0,12]
-        // i = 4 , poniner = 2
-        // [1,3,12,0,0]
-        // to find the non-zero values in the array
-        for(int i = 0; i < nums.length; i++) {
-            if(nums[i] != 0) {
-                int temp = nums[i];
-                nums[i] = nums[pointer];
-                nums[pointer] = temp;
-                pointer++;
+        int i = 0; 
+        for (int num:nums){
+            if(num != 0){
+                nums[i] = num;
+                i++;
             }
-        } 
-        
+        }
+        while(i<nums.length){
+            nums[i] = 0;
+            i++;
+        }
     }
 }
