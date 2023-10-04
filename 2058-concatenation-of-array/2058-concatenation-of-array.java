b9bf4e9937daaea1db5b21a70f4e4c18ede121ff
Jaydon Lee <96518885+Hyukjoo-Lee@users.noreply.github.com>
2023-10-04 03:08:13 -0700

    Time: 1 ms (96.58%), Space: 43.8 MB (96.67%) - LeetHub

--- 2058-concatenation-of-array/2058-concatenation-of-array.java
+++ 2058-concatenation-of-array/2058-concatenation-of-array.java
@@ -1,26 +1,16 @@
 class Solution {
     public int[] getConcatenation(int[] nums) {
-        /**
-            ans[i] == nums[i]
-            ans[i + n] == nums[i]
-            0-indexed
-
-            we can solve this problem simply looping nums array twice,
-            Can we solve this problem using only one loop?
-            
-         */
-        int[] answer = new int[nums.length * 2];
-        int n = nums.length;
-
-        for(int i = 0; i < answer.length; i++) {
-            if(i  < nums.length) {
-                answer[i] = nums[i];
-            } else {
-                answer[i] = nums[i - n];
-            }
-
+        // {1,2,3}
+        // {1,2,3,1,2,3}
+        int ansArrLength = nums.length * 2;
+        int[] ansArr = new int[ansArrLength];
+        // [0,0,0,0,0,0]
+        // [1,0,0,1,0,0]...
+        for(int i = 0; i < nums.length; i++) {
+            ansArr[i] = nums[i];
+            ansArr[i + nums.length] = nums[i];
         }
 
-        return answer;
+        return ansArr;
     }
 }
