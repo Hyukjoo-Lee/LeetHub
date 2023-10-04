da026c2ee8df6796825c2467f5da521b9fc3aa03
Jaydon Lee <96518885+Hyukjoo-Lee@users.noreply.github.com>
2023-10-04 02:56:16 -0700

    Time: 1 ms (98.42%), Space: 44.4 MB (40.36%) - LeetHub

--- 2048-build-array-from-permutation/2048-build-array-from-permutation.java
+++ 2048-build-array-from-permutation/2048-build-array-from-permutation.java
@@ -1,19 +1,21 @@
 class Solution {
-
-    //using recursion
     public int[] buildArray(int[] nums) {
-        aPermutation(nums,0);
+        // Permutation (순열) = 원래의 데이터들을 순서는 고려한 채 선별
+        // 다른 순서로 뒤섞음 
 
-        return nums;
-    }
-    
-    void aPermutation(int[] nums,int start){
-        if(start < nums.length){
-            int temp=nums[start];
-            int result=nums[temp];
-            aPermutation(nums,start+1);
-            nums[start]=result; // how it works?
+        // [0,5,1,3,2,4] 
+        // ans[0] = nums[nums[0]] = 0
+        // ans[1] = nums[nums[1]] = 2
+        // ans[2] = nums[nums[2]] = 5
+        // ...
+
+        int[] ans = new int[nums.length];
+
+        for(int i = 0; i < nums.length; i++) {
+            ans[i] = nums[nums[i]];
         }
-  
+
+        return ans;
+
     }
 }
