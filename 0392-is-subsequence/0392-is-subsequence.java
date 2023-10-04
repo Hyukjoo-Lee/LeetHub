6b041b328890d0f8d5ab59c993a65754190e35b4
Jaydon Lee <96518885+Hyukjoo-Lee@users.noreply.github.com>
2023-10-04 00:02:03 -0700

    Time: 0 ms (100%), Space: 40.8 MB (11.41%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,39 +1,45 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
         /**
-            We need to set a kind of position rule for i, j pointer
-            i => s index
-            j => t index
-
-            axc
-            axcgdc
+            s is subsequence of t
+            relative positions?
+            
+            j => idxT
+            i => idxS
+            abcde
+            ace (true)
+                j
+              i - when i reaches at the length of s string, check the if idxS == length
+            aec (false)
+                j - when j reaches at the length of t string, but i is not, false
               i
-              j
-           i == s.length means s is a subsequence of t !
          */
-        
-        char[] charArrS = s.toCharArray(); // axc
-        char[] charArrT = t.toCharArray(); // ahdgdc
 
-        int sIdx = 0;
+        char[] charArrS = s.toCharArray();
+        char[] charArrT = t.toCharArray();
 
-        for(int tIdx = 0; tIdx < charArrT.length && sIdx < charArrS.length; tIdx++) {
-            if(charArrS[sIdx] == charArrT[tIdx]) {
-                sIdx++;
+        int idxS = 0;
+
+        // abcde
+        // ace 
+        //      T
+        //    S 
+        for(int idxT = 0; idxT < charArrT.length && idxS < charArrS.length; idxT++) {
+            if(charArrS[idxS] == charArrT[idxT]) {
+                idxS++;
             }
         }
-        
-        return sIdx == charArrS.length;
+
+        return idxS == charArrS.length;
+
+
+
+
+
+
+
+
+
 
     }
 }
-
-
-
-
-
-
-
-
-
-
