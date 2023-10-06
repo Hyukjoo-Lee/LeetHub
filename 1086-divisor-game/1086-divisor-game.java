62fdecdf8a88db00b4dc79764f9c3223c2f35cd0
Jaydon Lee <96518885+Hyukjoo-Lee@users.noreply.github.com>
2023-10-06 15:23:24 -0700

    Time: 0 ms (100%), Space: 39.3 MB (25.72%) - LeetHub

---
+++ 1086-divisor-game/1086-divisor-game.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean divisorGame(int n) {
+        /**
+            Alice
+            Bob
+            chalkboard??
+            0 < x < n 
+            n % x == 0
+            2 % 2 == 0
+            3 % 1 == 0
+            cannot make a move, lose the game
+
+
+         */
+
+         return n % 2 == 0;
+    }
+}
