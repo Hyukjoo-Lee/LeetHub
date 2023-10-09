68d35e62cf67d7853ba45b5c1bf505e518071ddc
Jaydon Lee <96518885+Hyukjoo-Lee@users.noreply.github.com>
2023-10-09 15:36:20 -0700

    Time: 45 ms (32.3%), Space: 43.8 MB (24.13%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,36 +1,29 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         /**
-            nums   [2, 1, 5, 9]
-            target =  14
-            nums[0] + nums[1]
-            nums[0] + nums[2]
-            nums[0] + nums[3]
+            nums [2,7,11,15] target = 9
+            nums[0] + nums[0+1]
+            nums[0] + nums[0+2]
+            nums[0] + nums[0+3]
             ...
-            nums[2] + nums[3] = 14
-            return true
-
-            for (int i = 0; i < nums.length; i++)
-
-                for (int j = i+1; j < nums.length; j++)
-                    
-
+            nums[2] + nums[3]
          */
-         // nums[0] + nums[0+1]
-         // nums[0] + nums[0+2]
-         // nums[0] + nums[0+3]
-         // nums[1] + nums[2]
-         // nums[2] + nums[3]
-         for(int i = 0; i < nums.length; i++) {
-             for(int j = i + 1; j < nums.length; j++) {
-                 if(nums[i] + nums[j] == target) {
-                     return new int[] {i,j};
-                 }
-             }
-         }
 
+        
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = i+1; j < nums.length; j++) {
+                if(nums[i]+nums[j]==target) return new int[] {i,j};
+            }
+        }
 
         return null;
+
+
+
+
+
+
+
 
 
 
