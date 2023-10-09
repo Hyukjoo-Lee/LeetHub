2e4fdaf9a95628fe7a05f71cf1f093061812967b
Jaydon Lee <96518885+Hyukjoo-Lee@users.noreply.github.com>
2023-10-09 16:41:15 -0700

    Time: 16 ms (24.12%), Space: 43.9 MB (12.09%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,40 +1,22 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        /**
-            HashMap (count char)
-            anagram
-            [a:3, n:1, g:1, m:1 ,r:1]
-            nagaram
-         */
-         if(s.length() != t.length()) return false;
+        if (s.length() != t.length()) {
+            return false;
+        }
 
-         Map<Character, Integer> map = new HashMap<>();
-        
-         for(int i = 0; i < s.length(); i++) {
-           char current = s.charAt(i);
-           map.put(current, map.getOrDefault(current, 0) + 1);
-         }
-         // [a:0, n:0, g:0, m:0, r:0]
-         // nagaram
-         for(int i = 0; i < t.length(); i++) {
-           char current = t.charAt(i);
-           map.put(current, map.getOrDefault(current, 0) - 1);
-         }
-         
-         for(Map.Entry<Character,Integer> entry : map.entrySet()) {
-           Integer value = entry.getValue();
-           if(value != 0) return false;
-         }
-         
-         return true;
+        Map<Character, Integer> charCount = new HashMap<>();
 
+        for (char ch : s.toCharArray()) {
+            charCount.put(ch, charCount.getOrDefault(ch, 0) + 1);
+        }
 
+        for (char ch : t.toCharArray()) {
+            if (!charCount.containsKey(ch) || charCount.get(ch) == 0) {
+                return false;
+            }
+            charCount.put(ch, charCount.get(ch) - 1);
+        }
 
-
-
-
-
-
-
+        return true;
     }
 }
