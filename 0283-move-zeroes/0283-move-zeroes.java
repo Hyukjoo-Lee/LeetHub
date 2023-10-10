31dfdbf15380aa08803717a1ec0b1f21a768dd55
Jaydon Lee <96518885+Hyukjoo-Lee@users.noreply.github.com>
2023-10-10 00:44:46 -0700

    Time: 1 ms (100%), Space: 45.1 MB (50.61%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,15 +1,41 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int i = 0; 
-        for (int num:nums){
-            if(num != 0){
-                nums[i] = num;
-                i++;
+        /**
+            move all 0's to the end of nums array
+            maintain the relative order
+            in-place 
+            fix start position
+            [0,1,0,3,12]
+         */
+
+        int j = 0;
+        // [0,1,0,3,12]
+        //    i
+        //  j
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] != 0) {
+                nums[j] = nums[i];
+                j++;
             }
         }
-        while(i<nums.length){
-            nums[i] = 0;
-            i++;
+
+        while(j < nums.length) {
+            nums[j++] = 0;
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
