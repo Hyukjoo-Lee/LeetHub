50ab1be647f2df21988ed332f734225868934088
Jaydon Lee <96518885+Hyukjoo-Lee@users.noreply.github.com>
2023-10-09 17:31:08 -0700

    Time: 1 ms (98.24%), Space: 44 MB (91.8%) - LeetHub

--- 2048-build-array-from-permutation/2048-build-array-from-permutation.java
+++ 2048-build-array-from-permutation/2048-build-array-from-permutation.java
@@ -1,21 +1,16 @@
 class Solution {
     public int[] buildArray(int[] nums) {
-        // Permutation (순열) = 원래의 데이터들을 순서는 고려한 채 선별
-        // 다른 순서로 뒤섞음 
+        /**
+            ans[i] = nums[nums[i]]
+            simply using this formula
+         */
 
-        // [0,5,1,3,2,4] 
-        // ans[0] = nums[nums[0]] = 0
-        // ans[1] = nums[nums[1]] = 2
-        // ans[2] = nums[nums[2]] = 5
-        // ...
+         int[] ans = new int[nums.length];
 
-        int[] ans = new int[nums.length];
+         for(int i = 0; i < nums.length; i++) {
+             ans[i] = nums[nums[i]];
+         }
 
-        for(int i = 0; i < nums.length; i++) {
-            ans[i] = nums[nums[i]];
-        }
-
-        return ans;
-
+         return ans;
     }
 }
