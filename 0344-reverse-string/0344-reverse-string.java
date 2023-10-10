b7704904bf544b810dd0bf6546d6b5bde46b3524
Jaydon Lee <96518885+Hyukjoo-Lee@users.noreply.github.com>
2023-10-10 01:02:38 -0700

    Time: 0 ms (100%), Space: 50.6 MB (49.42%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,24 +1,9 @@
 class Solution {
     public void reverseString(char[] s) {
-        /**
-            reverse the given string 
-            in-place
-            O(1)
-
-            reverse function..
-            or
-            two pointers / stack
-         */
-
-         Stack<Character> stack = new Stack<>();
-
-         for(char ch : s) {
-             stack.push(ch);
-         }
-        
-         int i = 0;
-         while(!stack.isEmpty()) {
-             s[i++] = stack.pop();
-         }
+        for(int i = 0; i < s.length / 2; i++) {
+            char swap = s[i];
+            s[i] = s[s.length - 1 - i];
+            s[s.length - 1 - i] = swap;
+        }
     }
 }
