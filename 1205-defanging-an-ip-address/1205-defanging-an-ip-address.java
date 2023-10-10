5e076847998c58a90aaa1b5188ddebc84e254c7d
Jaydon Lee <96518885+Hyukjoo-Lee@users.noreply.github.com>
2023-10-09 17:13:17 -0700

    Time: 0 ms (100%), Space: 40.5 MB (48.53%) - LeetHub

---
+++ 1205-defanging-an-ip-address/1205-defanging-an-ip-address.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String defangIPaddr(String address) {
+        /**
+            replace every period with [.]
+            1. simply utilizing replace function address.replace(".", "[.]")
+
+            2. find . and replace [.] (sb or concatenation)
+
+         */
+
+        return address.replace(".", "[.]");
+
+
+
+
+
+
+
+
+
+
+    }
+}
