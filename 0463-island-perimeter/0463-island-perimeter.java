b95aba603eb40f5c0ed974a5ce5de8d1bd4ca35b
Jaydon Lee <96518885+Hyukjoo-Lee@users.noreply.github.com>
2023-10-10 21:00:32 -0700

    Time: 6 ms (41.44%), Space: 44.7 MB (8.16%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.java
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -1,40 +1,43 @@
 public class Solution {
     public int islandPerimeter(int[][] grid) {
-        /**
-            grid[i][j] = 1 : land
-            grid[i][j] = 0 : water
-
-            if grid surrounded by water, + 4
-            if grid surrounded by land, + 0
-
-            we can access the top/bottom/left/right
-            and determine if the side is 0 or not,
-            if 0, + 1 otherwise 0
-
-         */
-
-        int perimeter = 0;
         int rows = grid.length;
         int cols = grid[0].length;
 
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 if (grid[i][j] == 1) {
-                    // assume that all sides of the current cell are water
-                    perimeter += 4; 
-
-                    // check if left cell is land
-                    if (i > 0 && grid[i - 1][j] == 1) {
-                        perimeter -= 2; 
-                    }
-                    
-                    // check if bottom cell is land
-                    if (j > 0 && grid[i][j - 1] == 1) {
-                        perimeter -= 2; // bottom
-                    }
+                    return dfs(grid, i, j);
                 }
             }
         }
+
+        return 0;
+    }
+
+    private int dfs(int[][] grid, int row, int col) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        if (row < 0 || row >= rows || col < 0 || col >= cols) {
+            return 1;
+        }
+
+        if (grid[row][col] == 0) {
+            return 1;
+        }
+
+        if (grid[row][col] == -1) {
+            return 0;
+        }
+
+        // Mark the cell as visited
+        grid[row][col] = -1;
+
+        int perimeter = 0;
+        perimeter += dfs(grid, row - 1, col);
+        perimeter += dfs(grid, row + 1, col);
+        perimeter += dfs(grid, row, col - 1);
+        perimeter += dfs(grid, row, col + 1);
 
         return perimeter;
     }
