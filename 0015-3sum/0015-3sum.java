0471dbc821cb31d53f26f9be3142e7ae25b54d21
Jaydon Lee <96518885+Hyukjoo-Lee@users.noreply.github.com>
2023-11-01 22:59:12 -0700

    Time: 457 ms (13.47%), Space: 50.1 MB (89.38%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,61 +1,46 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         /**
-            find all the triplets 
-            nums[i] + nums[j] + nums[k] == 0
+            return all triplets that sum equals to 0
+
             must not contain duplicate triplets
+            => Set
 
-            sort nums 
-            3 pointers?
-            i = 0, start = i+1, end = nums.length -1
-            [-4,-1,-1,0,1,2]
-                i
-                   s      e
-            -4 -1 +2 = -3
-            if sum -3 < 0, start ++
-            if sum    > 0, end--
-            if sum == 0, list.addAll(Arrays.asList(nums[i], nums[start], nums[end]))
-                
-        
+            3 pointers 
+            i, start , end
+            
+            to be sorted
+            
+            [-2,-1,0,1,2,3]
+             i. s        e
+
+            ** method takes the array 'a' which is required to be converted into a List. 
+                Arrays.asList(a)
          */
+        
+        Set<List<Integer>> set = new HashSet<>();
 
+        Arrays.sort(nums);
+        int sum = 0;
 
-         // To remove duplicate triplets
-         Set<List<Integer>> answer = new HashSet<>();
+        for(int i = 0; i < nums.length; i++) {
+            int start = i + 1;
+            int end = nums.length - 1;
 
-         Arrays.sort(nums);
+            while(start < end) {
+                sum = nums[i] + nums[start] + nums[end];
+                if(sum == 0) {
+                    set.add(Arrays.asList(nums[i], nums[start], nums[end]));
+                    start++;
+                    end--;
+                } else if (sum < 0) {
+                    start++;
+                } else {
+                    end--;
+                }
+            } 
+        }
 
-         for(int i = 0; i < nums.length -2; i++) {
-           int start = i +1;
-           int end = nums.length -1;
-
-           while(start < end) {
-             int sum = nums[i] + nums[start] + nums[end];
-             if(sum == 0) {
-               answer.add(Arrays.asList(nums[i], nums[start], nums[end]));
-               start++;
-               end--;
-             } else if(sum < 0) {
-               start++;
-             } else {
-               end--;
-             }
-           }
-         }
-
-        return new ArrayList<>(answer);
-
-
-
-
-
-
-
-
-
-
-
-
-
+        return new ArrayList<>(set);
     }
 }
