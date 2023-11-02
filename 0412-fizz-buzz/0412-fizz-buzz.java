2d3ba30459bab22310807850b00efb55fceacb80
Jaydon Lee <96518885+Hyukjoo-Lee@users.noreply.github.com>
2023-11-02 00:11:55 -0700

    Time: 1 ms (99.91%), Space: 44.3 MB (79.92%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.java
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -1,37 +1,40 @@
 class Solution {
     public List<String> fizzBuzz(int n) {
         /**
-            n
-            return string array (1-indexed)
+        
+            integer n
+            return a string array answer
 
-            answer[i] = "FizzBuzz" if i is divisible by 3 && 5
-                      = "Fizz"     if i is divisible by 3
-                      = "Buzz"     if i is divisible by 5
-                      = (as string) remaining conditions
+            if index is divisible by 3 and 5
+            add FizzBuzz
+            
+            if index is divisible by 3
+            add Fizz
 
+            if index is divisible by 5
+            add Buzz
 
-           answer[3] = 1,2,"Fizz"
+            n = 7
+            [1,2,Fizz,4,Buzz,Fizz,7..]
 
-           return a string array? 
-
-           But return type is List<String>
-           
+            
+        
          */
          
-         List<String> answer = new ArrayList<>();
-         
+         List<String> list = new ArrayList<>();
+
          for(int i = 1; i <= n; i++) {
-             if(i % 3 == 0 && i % 5 ==0) {
-                 answer.add("FizzBuzz");
-             } else if (i % 3 == 0) {
-                 answer.add("Fizz");
-             } else if (i % 5 == 0) {
-                 answer.add("Buzz");
-             } else {
-                 answer.add(Integer.toString(i));
-             }
+            if(i % 3 == 0 && i % 5 == 0) {
+                list.add("FizzBuzz");
+            } else if(i % 5 == 0) {
+                list.add("Buzz");
+            } else if(i % 3 == 0) {
+                list.add("Fizz");
+            } else {
+                list.add(Integer.toString(i));
+            }
          }
 
-         return answer;
+         return list;
     }
 }
