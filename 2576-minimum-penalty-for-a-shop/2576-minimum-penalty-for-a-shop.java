436e6cca08aad205f4deed77184548710f875697
Jaydon Lee <96518885+Hyukjoo-Lee@users.noreply.github.com>
2023-11-07 18:03:58 -0800

    Time: 17 ms (11.66%), Space: 44.5 MB (22.58%) - LeetHub

---
+++ 2576-minimum-penalty-for-a-shop/2576-minimum-penalty-for-a-shop.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int bestClosingTime(String customers) {
+        int n = customers.length();
+        int[] openCount = new int[n];
+        int[] closeCount = new int[n];
+
+        // Calculate cumulative counts from left to right
+        for (int i = 0; i < n; i++) {
+            openCount[i] = (i > 0 ? openCount[i - 1] : 0) + (customers.charAt(i) == 'N' ? 1 : 0);
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            closeCount[i] = (i < n - 1 ? closeCount[i + 1] : 0) + (customers.charAt(i) == 'Y' ? 1 : 0);
+        }
+
+        int minPenalty = Integer.MAX_VALUE;
+        int bestClosingHour = -1;
+
+        for (int j = 0; j <= n; j++) {
+            int penalty = (j > 0 ? openCount[j - 1] : 0) + (j < n ? closeCount[j] : 0);
+
+            if (penalty < minPenalty) {
+                minPenalty = penalty;
+                bestClosingHour = j;
+            }
+        }
+
+        return bestClosingHour;
+    }
+}
