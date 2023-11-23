af65d2e7f8e596ed41fc8a26d751a902d5caa58e
Jaydon Lee <96518885+Hyukjoo-Lee@users.noreply.github.com>
2023-11-22 20:25:59 -0800

    Time: 15 ms (50.79%), Space: 43 MB (50.78%) - LeetHub

---
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int closestSum = nums[0] + nums[1] + nums[2];
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            int start = i + 1;
+            int end = nums.length - 1;
+
+            while (start < end) {
+                int sum = nums[i] + nums[start] + nums[end];
+                if (Math.abs(target - sum) < Math.abs(target - closestSum)) {
+                    closestSum = sum;
+                }
+
+                if (sum > target) {
+                    end--;
+                } else {
+                    start++;
+                }
+            }
+        }
+        return closestSum;
+    }
+
+}
