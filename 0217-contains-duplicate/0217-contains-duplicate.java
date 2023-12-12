f1b1d46b681b8210ed36bd59ad23e4a2494b3224
Jaydon Lee <96518885+Hyukjoo-Lee@users.noreply.github.com>
2023-12-11 18:39:08 -0800

    Time: 10 ms (83.54%), Space: 55.3 MB (55.28%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,10 +1,24 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         /**
-            using set
-            if(set.contains(nums[i]))
+            sort => compare
+
+            
          */
+        // Approach 1 =  sort => two pointers => O(nlogn) => time limit exceeded
+
+        //  Arrays.sort(nums);
+
+        //  for(int i = 0; i < nums.length -1; i++) {
+        //      for(int j = i + 1; j < nums.length; j++) {
+        //          if(nums[i] == nums[j]) return true;
+        //      }
+        //  }
+
+        //  return false;
         
+        // Approach 2 = Set => O(N)
+
         Set<Integer> set = new HashSet<>();
 
         for(int num : nums) {
@@ -13,10 +27,6 @@ class Solution {
         }
 
         return false;
-
-
-
-
 
     }
 }
