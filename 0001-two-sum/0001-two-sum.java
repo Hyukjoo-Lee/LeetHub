33fdc324b336a441c8744e59ac24bd4689d69a5b
Jaydon Lee <96518885+Hyukjoo-Lee@users.noreply.github.com>
2023-12-11 18:31:07 -0800

    Time: 2 ms (81.51%), Space: 44.1 MB (9.25%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,40 +1,28 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         /**
-            nums [2,7,11,15] target = 9
-            [2:0] [7:1] [11:2] [15:3]
-            if(containsKey(7)) {
-                return new int[] {map.get(7), i}
-            }
-            [2:0] [7:1]
-            7 = target - nums[i]
-            x
-            x = target - nums[i]
+            [2,7,11,15] target = 9
+
+            [0,1]
+
+            [2:0, 7:1, 11:2, 15:3]  
+
+            9 - 2 = 7
+            target - nums[0] = nums[1]
+
+            return [0,1]
+
          */
+         Map<Integer, Integer> map = new HashMap<>();
 
-        Map<Integer, Integer> map = new HashMap<>();
-        // x = 9 - 2
-        // [2:0]
-        // x = 9 - 7 = 2
+         for(int i = 0; i < nums.length; i++) {
+             int x = target - nums[i];
+             if(map.containsKey(x)) {
+                 return new int[] {i, map.get(x)};
+             }
+             map.put(nums[i], i);
+         }
 
-        for(int i = 0; i < nums.length; i++) {
-            int x = target - nums[i];
-            if(map.containsKey(x)) {
-                return new int[] {map.get(x), i};
-            }
-
-            map.put(nums[i], i);
-        }
-         
-        return null;
-
-
-
-
-
-
-
-
-
+         return new int[] {};
     }
 }
