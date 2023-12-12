c08187d88a4923bcfdc07220035e788fd234efec
Jaydon Lee <96518885+Hyukjoo-Lee@users.noreply.github.com>
2023-12-11 19:00:00 -0800

    Time: 3 ms (89.38%), Space: 44.1 MB (11.74%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,21 +1,14 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        /**
-            simply sort the s and t
-            and determine if both strings are equal
-            
-            HashMap (count char)
-         */
+        char[] chS = s.toCharArray();
+        char[] chT = t.toCharArray();
+        
+        Arrays.sort(chS);
+        Arrays.sort(chT);
 
-        char[] ch1 = s.toCharArray();
-        char[] ch2 = t.toCharArray();
+        s = new String(chS);
+        t = new String(chT);
 
-        Arrays.sort(ch1);
-        Arrays.sort(ch2);
-
-        String sortedStS = new String(ch1);
-        String sortedStT = new String(ch2);
-
-        return sortedStS.equals(sortedStT);
+        return s.equals(t);
     }
 }
