cfccfec75b991be0f1e37b59a1b56f0fbfd803bd
Jaydon Lee <96518885+Hyukjoo-Lee@users.noreply.github.com>
2024-05-08 00:20:40 +0900

    Time: 7 ms (99.35%), Space: 60.3 MB (31.23%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,32 +1,20 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         /**
-            sort => compare
-
+            시간 복잡도를 줄이기 위하여 HashSet 를 사용할 수 있습니다.
+            HashSet의 각 요소를 distint하게 저장하는 특성을 활용합니다.
+            이렇게 하면 시간 복잡도를 O(n) 까지 줄일 수 있습니다.
             
+            HashSet.add() method 는 저장 할 요소가 이미 set 에 존재하면 false 를 반환합니다.
          */
-        // Approach 1 =  sort => two pointers => O(nlogn) => time limit exceeded
-
-        //  Arrays.sort(nums);
-
-        //  for(int i = 0; i < nums.length -1; i++) {
-        //      for(int j = i + 1; j < nums.length; j++) {
-        //          if(nums[i] == nums[j]) return true;
-        //      }
-        //  }
-
-        //  return false;
+         
+         Set<Integer> set = new HashSet<>();
         
-        // Approach 2 = Set => O(N)
+         for(int num : nums) {
+            // 중복 발견
+            if(!set.add(num)) return true;
+         }
 
-        Set<Integer> set = new HashSet<>();
-
-        for(int num : nums) {
-            if(set.contains(num)) return true;
-            set.add(num);
-        }
-
-        return false;
-
+         return false;
     }
 }
