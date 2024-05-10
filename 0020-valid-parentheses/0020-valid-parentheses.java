48df280b2cf4cfd78982704f3c30ff1c3daba505
Jaydon Lee <96518885+Hyukjoo-Lee@users.noreply.github.com>
2024-05-10 17:47:32 +0900

    Time: 1 ms (98.65%), Space: 41.2 MB (74.66%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,45 +1,41 @@
+import java.util.Stack;
+
 class Solution {
     public boolean isValid(String s) {
         /**
-            1. stack - LIFO
-            "()[]{}"
-            push (
-            if ")", stack.pop();
-            push [
-            if "]", stack.pop();
-            push {
-            if "}", stack.pop();
+            Open brackets , close brackets 으로만 이루어진 단어가 
 
-            return stack.isEmpty();
+            주어진 3개의 조건에 만족하는지 판단하는 문제입니다.
+
+            Stack 의 push 와 pop 의 속성 FILO 을 이용해서 문제를 해결합니다.
+
+            시간 복잡도는 문자열의 모든 문자를 한번씩 연산하기 때문에
+
+            O(N) 입니다.
+
+            여기서 주의 해야 할 점은
+
+            스택이 비어 있지 않은 상태에서 peek() 또는 pop()을 호출하기 전에 
+
+            isEmpty()를 사용하여 스택이 비어 있는지 확인해야 합니다.
+
          */
-
-        char[] chArr = s.toCharArray();
-        
         Stack<Character> stack = new Stack<>();
 
-        for(Character ch : chArr) {
-            // ( [ {
-            if(ch == '(' || ch == '{' || ch== '[') {
-                stack.push(ch);                
-            } else if(ch == ')' && !stack.isEmpty() && stack.peek() == '(') {
-                stack.pop();
-            } else if(ch == '}' && !stack.isEmpty() && stack.peek() == '{') {
-                stack.pop();
-            } else if(ch == ']' && !stack.isEmpty() && stack.peek() == '[') {
-                stack.pop();
+        for(char ch : s.toCharArray()) {
+            if(ch == '(' || ch == '[' || ch == '{') {
+                stack.push(ch);
             } else {
-                return false;
+                if (stack.isEmpty()) {
+                    return false; 
+                }
+                char top = stack.pop();
+                if ((ch == ')' && top != '(') || (ch == ']' && top != '[') || (ch == '}' && top != '{')) {
+                    return false; 
+                }
             }
         }
 
         return stack.isEmpty();
-
     }
-
-
-
-
-
-
-
 }
