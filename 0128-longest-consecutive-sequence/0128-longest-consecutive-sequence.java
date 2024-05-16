1cf43b6db0a55253b762aa06dc462d9151d8a144
Jaydon Lee <96518885+Hyukjoo-Lee@users.noreply.github.com>
2024-05-16 20:56:56 +0900

    Time: 27 ms (74.32%), Space: 67.1 MB (22.69%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,39 +1,41 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
         /**
-            [100,4,200,1,2,3]
+            Telus Health final 코테 기출문제
 
-            brute force.. more than n^2
-            sort => O(logn)..
-
-            to implement O(n), HashSet
-            distinct numbers, hash (index X)
-         */
-
-        Set<Integer> set = new HashSet<Integer>();
+            정렬되지 않은 정수 배열이 주어지고
+            '최장 공통 부분 수열 = LCS' 의 길이를 반환하는 문제입니다.
         
-        // to remove the duplicated elements 
+            솔루션의 time complexity가 O(N) 이여야 합니다.
+            예로 [100,4,200,1,3,2] 가 주어지면
+            요소중에서 [1,2,3,4] 가 LCS 이므로 정답은 4가 됩니다.
+            
+            정렬해서 최장길이를 판단하는 방법이 있고
+            HashSet을 활용하여 중복 제거를 한 후 최장길이를 판단하는 방법이 있습니다.
+
+            Constraints 로 O(N) 의 time complexity 가 주어졌으니 두번째 방법이 적합합니다.
+         */
+        Set<Integer> set = new HashSet<>();
+
         for(int num : nums) {
             set.add(num);
         }
 
-        int updatedLongest = 0;
-        
+        int longest = 0;
+
         for(int num : set) {
-            // check if num is starting point
-            if(!set.contains(num - 1)) {
+            if(!set.contains(num-1)) {
                 int currentNum = num;
-                int currentLongest = 1;
-
-                while(set.contains(currentNum + 1)) {
+                // 초기화
+                int currentSequence = 1;
+                while(set.contains(currentNum+1)) {
                     currentNum++;
-                    currentLongest++;
+                    currentSequence++;
                 }
-
-                updatedLongest = Math.max(updatedLongest,currentLongest);
+                longest = Math.max(longest, currentSequence);
             }
         }
 
-        return updatedLongest;
+        return longest;
     }
 }
