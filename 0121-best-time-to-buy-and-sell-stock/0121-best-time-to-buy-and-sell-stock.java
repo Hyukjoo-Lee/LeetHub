bc100af5f52405d69ebe633218a8c53827e4ac83
Jaydon Lee <96518885+Hyukjoo-Lee@users.noreply.github.com>
2024-05-21 20:32:20 +0900

    Time: 2 ms (77.41%), Space: 61.7 MB (32.69%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,36 +1,25 @@
 class Solution {
     public int maxProfit(int[] prices) {
         /**
-            an array prices
-            i days day
-
-            [7,1,5,3,6,4]
-                 i
-                       j
-             maximize the profit choosing a single to buy one stock
-                                    ''    a different day in the future to sell stock
-
-            return maxProfit 
-            you cannot, return 0
-
-
-            current = maxprice - minprice
-
+            주어진 가격 prices 가 주어졌을 때,
+            주식을 사고 팔아 얻을 수 있는 최대 수익을 계산하고 반환하는 문제입니다.
+            
+            
+            최소 가격(minPrice)을 계속 갱신하며
+            현재 가격과 최소 가격의 차이를 이용해 최대 수익(maxProfit)을 계산합니다.
+            만약 이익을 낼 수 없는 경우 0을 반환합니다.
          */
          
-         int maxProfit = Integer.MIN_VALUE;
+         int maxProfit = 0;
+         
+         // 첫 번째 날의 가격
          int minPrice = prices[0];
 
+         // 첫 번째 날 이후의 모든 날에 대한 연산 
          for(int i = 1; i < prices.length; i++) {
              if(minPrice > prices[i]) minPrice = prices[i];
-
              int current = prices[i] - minPrice;
-
              if(maxProfit < current) maxProfit = current;
-         }
-         
-         if(maxProfit < 0) {
-             maxProfit = 0;
          }
 
          return maxProfit;
