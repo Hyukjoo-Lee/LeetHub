2898cc5a44bc47e740e9990a96e71d2b381ef8b5
Jaydon Lee <96518885+Hyukjoo-Lee@users.noreply.github.com>
2024-05-24 23:32:56 +0900

    Time: 0 ms (100.00%), Space: 42.1 MB (85.16%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -9,24 +9,48 @@
  * }
  */
 class Solution {
-    // using 3 pointers ; current, prev, temp 
-    // to keep track of nodes to update reserved node
     public ListNode reverseList(ListNode head) {
-        // initialize current and prev node
-        ListNode current = head;
-        ListNode prev = null;
-        while(current != null) {
-            // temporaily store the next node of current node
-            ListNode temp = current.next;
-            // change pointer from -> to <- 
-            current.next = prev;
-            // move prev node to current node
-            prev = current;
-            // move current node to temp node
-            current = temp;
-        }
-        // finally store prev node to head
-        head = prev;
-        return head;
+        /**
+            간단하게 linked list 를 reverse 하는 문제
+
+            prev 노드는 전 노드를 추적할거고
+            cur 노드는 리스트를 looping 하기 위한 노드
+            temp 노드는 cur 노드의 다음 노드를 일시적을 저장하기 위해 사용됩니다.
+
+            예를 들어
+
+            null -> 1 -> 2 -> 3 -> null 이 주어진다면
+            prev.  cur. temp 로 초기화하고
+            다음 loop 에서 전 노드를 추적하기 위한 수단으로 temp 를 cur.next 로 저장 후
+                 <-
+            1. cur.next 를 prev 로 (reverse pointer)
+
+            2. prev 노드 이동 -> cur 로
+
+            3. cur = temp ... 반복
+
+            마지막에 head 를 prev 로
+
+            그리고 head 를 리턴합니다.
+
+            time complexity 는 모든 노드를 방문하기 때문에 O(N),
+
+            space complextiy 는 몇가지 변수만 사용 되었기 때문에 O(1) 입니다.
+         */
+
+         ListNode cur = head;
+         ListNode prev = null;
+
+         while(cur != null) {
+            // 임시적으로 cur 노드의 다음 노드를 저장
+            ListNode temp = cur.next;
+            cur.next = prev; // 1
+            prev = cur; // 2
+            cur = temp; // 3
+         }
+
+         head = prev;
+         return head;
+         
     }
 }
